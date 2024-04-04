bf80d3a810e27847c40f2b46d86a45055edff8df
Enantiomorph <optionalnodes@gmail.com>
2024-04-04 17:49:20 +0800

    Update main.go

--- main.go
+++ main.go
@@ -1,41 +1,77 @@
 package main
 
 import (
+	"flag"
 	"fmt"
+	"log"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
 	"time"
 )
 
 func main() {
-	url := "http://example.com"
-	cookie := &http.Cookie{
-		Name:   "your_cookie_name",
-		Value:  "your_cookie_value",
-		Path:   "/",
-		Domain: ".example.com",
-	}
-	requestInterval := 5 * time.Second
+	// 解析命令行参数
+	var (
+		url            string
+		cookieName     string
+		cookieValue    string
+		requestInterval int
+	)
+	flag.StringVar(&url, "url", "http://example.com", "Target URL to send requests to")
+	flag.StringVar(&cookieName, "cookie-name", "your_cookie_name", "Name of the cookie")
+	flag.StringVar(&cookieValue, "cookie-value", "your_cookie_value", "Value of the cookie")
+	flag.IntVar(&requestInterval, "interval", 5, "Interval between requests in seconds")
+	flag.Parse()
 
-	client := &http.Client{}
+	// 设置请求间隔
+	intervalDuration := time.Duration(requestInterval) * time.Second
 
-	for {
-		req, err := http.NewRequest("GET", url, nil)
-		if err != nil {
-			fmt.Println("Error creating request:", err)
-			return
+	// 准备优雅终止
+	signals := make(chan os.Signal, 1)
+	signal.Notify(signals, os.Interrupt, syscall.SIGTERM)
+	done := make(chan bool, 1)
+
+	// 启动请求循环
+	go func() {
+		client := &http.Client{}
+		for {
+			select {
+			case <-done:
+				return
+			default:
+				// 创建带有Cookie的请求
+				req, err := http.NewRequest("GET", url, nil)
+				if err != nil {
+					log.Println("Error creating request:", err)
+					continue
+				}
+				cookie := &http.Cookie{
+					Name:   cookieName,
+					Value:  cookieValue,
+					Path:   "/",
+					Domain: url,
+				}
+				req.AddCookie(cookie)
+
+				// 发送请求
+				resp, err := client.Do(req)
+				if err != nil {
+					log.Println("Error sending request:", err)
+					continue
+				}
+				log.Printf("Received response with status code: %d\n", resp.StatusCode)
+				resp.Body.Close()
+
+				// 等待指定的时间间隔
+				time.Sleep(intervalDuration)
+			}
 		}
+	}()
 
-		req.AddCookie(cookie)
-
-		resp, err := client.Do(req)
-		if err != nil {
-			fmt.Println("Error sending request:", err)
-			return
-		}
-		fmt.Printf("Received response with status code: %d\n", resp.StatusCode)
-
-		resp.Body.Close()
-
-		time.Sleep(requestInterval)
-	}
+	// 等待信号量以优雅地终止程序
+	<-signals
+	done <- true
+	log.Println("Program terminated gracefully")
 }
